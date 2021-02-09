f29d38f456b867f9cd5458262c4661e70dc727d0
Mina Abbasi <minaabbasi@Minas-MacBook-Air.local>
2021-02-09 17:05:00 -0500

    developed run_wgame method

--- otello.cpp
+++ otello.cpp
@@ -1,3 +1,4 @@
+#include<utility>
 #include<iostream>
 #include<vector>
 
@@ -7,27 +8,9 @@ class Exception{};
 
 class Player{
 public:	
-	
-	Player(std::string name):name(name), number_of_disks(30){};	
+	Player(std::string name):name(name){};	
 	std::string name;
-	void borrow_disk(Player &opponent);
-	int get_number_of_disks() {return number_of_disks;};
-	
-private:
-	bool my_turn;
-	std::string color;
-	int number_of_disks;
 };
-
-void Player::borrow_disk(Player &opponent){
-	if (number_of_disks > 0 || !my_turn)
-		throw Exception();
-	if (opponent.number_of_disks){ 
-		opponent.number_of_disks--;
-		number_of_disks++;
-	}
-}
-
 
 
 class Board{
@@ -55,12 +38,25 @@ std::string Board::find_winer(){
 
 class InputInterface{
 public:
-	void get_player_name();
+	std::pair<int, int> get_move();
+};
+
+std::pair<int,int>InputInterface::get_move(){
+	std::pair<int,int> move =std::make_pair(-1,-1);
+	while(move.first > 7 || move.first < 0 || move.second > 7 || move.second < 0) {	
+		std::cin >> move.first >> move.second;
+		if(move.first > 7 || move.first < 0 || move.second > 7 || move.second < 0) {	
+			std::cout << "Invalid move, choose from the valid moves" << std::endl;
+		}
+	}
+	 
+	return move;
 };
 
 class OutputInterface{
 public:
 	void show_board(Board& board);
+	void show_available_moves(std::vector<std::pair<int,int>> available_moves, std::string players_name);
 };
 
 
@@ -74,18 +70,26 @@ void OutputInterface::show_board(Board& board){
 	}
 };
 
+void OutputInterface::show_available_moves(std::vector<std::pair<int,int>> available_moves, std::string players_name){
+	std::cout<< "Available moves for "<< players_name << ":" << std::endl;
+	for (auto move: available_moves)
+		std::cout << "("<< move.first <<"," << move.second << " ,";
+	std::cout<<std::endl;
+}
 
 
  
 class Otello{
 public:	
+	Otello():number_of_disks(64){};
 	void initialize_board(Board& board);
-	void run_game(OutputInterface out, Board& board);
+	void run_game(OutputInterface out,InputInterface in, Board& board, Player &black, Player &white);
 private:
-	void flip_disks();
+	int number_of_disks;
+	void flip_disks(std::pair<int,int> move, Board& board){};
 	void is_valid_move();
-	void find_valid_moves();
-	void change_turn();
+	std::vector<std::pair<int,int>> find_available_moves(Board &board, Player& current_player){};
+
 };
 
 void Otello::initialize_board(Board& board) {
@@ -93,15 +97,32 @@ void Otello::initialize_board(Board& board) {
 	board.board[3][4] = " B ";
 	board.board[4][3] = " B ";
 	board.board[4][4] = " W ";
+	number_of_disks -= 4;
+
 }
 
-void Otello::run_game(OutputInterface out, Board& board){
+
+void Otello::run_game(OutputInterface out, InputInterface in, Board& board, Player &black, Player &white){
 	initialize_board(board);
 	out.show_board(board);
+	std::vector<std::pair<int,int>> available_moves;
+	Player current_player = black;
+	std::cout << "set current player to black" << std::endl;
+	std::pair<int,int> current_move;
 	while (board.empty_cells){
+		//available_moves = find_available_moves(board, current_player);
+		out.show_available_moves(available_moves, current_player.name);
 		std::cout << board.empty_cells << std::endl;
-		board.empty_cells --;
-	
+		//if (!available_moves.empty())
+			board.empty_cells --;
+		current_move = in.get_move();
+		std::cout << current_move.first<< "," << current_move.second << std::endl;
+		flip_disks(current_move, board);	
+		out.show_board(board);
+		if (current_player.name == "Black")
+			current_player = white;
+		else
+			current_player = black;
 	}
 }
 
@@ -110,13 +131,10 @@ int main(){
 	Board board;
 	OutputInterface out;
 	InputInterface input;
-	game.run_game(out, board);
+	Player black("Black");
+	Player white("White");
+	game.run_game(out, input, board, black, white);
 
 	std::cout << board.find_winer() << std::endl;
 	
-	std::string black_name;
-	std::cout << "Enter name of black player:" << endl;
-	std::cin >> black_name;
-	Player black(black_name);
-	std::cout << black.name << " has " << black.get_number_of_disks() << " disks." << std::endl;
 }
